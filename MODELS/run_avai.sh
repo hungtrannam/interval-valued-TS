3dd46376a321b42fcaf646c476333576b866236a
Your Name <you@example.com>
2025-05-28 15:22:37 +0700

    [2025-05-28 15:22:37] Newest Version Updated

--- MODELS/run_avai.sh
+++ MODELS/run_avai.sh
@@ -1,4 +1,4 @@
 
-python run_avai2.py test_results/optuna_tune_VIC.csv_DLinear_trial4/best.yaml > hyper.sh
-source hyper.sh
+python run_avai.py test_results/optuna_tune_VIC.csv_DLinear_trial4/best.yaml > run_hyper.sh
+source run_hyper.sh
 
